a5ffd40463958a7500d32871294d89569b27b231
ilham-yale2 <ilhammaghfiro@gmail.com>
2025-04-16 10:50:56 +0700

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -13,6 +13,8 @@ export default defineConfigWithVueTs(
         rules: {
             'vue/multi-word-component-names': 'off',
             '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+            'vue/no-mutating-props': 'off',
         },
     },
     prettier,
